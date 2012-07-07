6d67947d96f3efa41f8595f08f309b70de217c34
greenleaf <greenleaf@greenleaf-Veriton-X2610>
2012-07-07 23:24:08 +0800

    task-1-2.

--- kernel-lib/include/Atomic.h
+++ kernel-lib/include/Atomic.h
@@ -35,4 +35,117 @@ public:
 private:
 	volatile int value;
 };
+/************************************************************************/
+/*					       读写操作
+/*                       Read Write
+/************************************************************************/
+inline int Atomic::Read()
+{
+	return value;
+}
+
+inline void Atomic::Write(int Value)
+{
+	this->value = Value;
+	return;
+}
+
+/************************************************************************/
+/*					       加减操作
+/*                         Add Sub
+/************************************************************************/
+inline void Atomic::Add(int Value)
+{
+	asm volatile("addl %1,%0"
+	             : "+m" (this->value)
+	             : "r" (Value));
+	return;
+}
+
+inline void Atomic::Sub(int Value)
+{
+	asm volatile("subl %1,%0"
+	             : "+m" (this->value)
+	             : "r" (Value));
+	return;
+}
+/************************************************************************/
+/*					      自加或自减运算
+/*                         Inc Dec
+/************************************************************************/
+inline void Atomic::Inc()
+{
+	asm volatile("incl %0"
+				: "+m" (this->value));
+	return;
+}
+
+inline void Atomic::Dec()
+{
+	asm volatile("decl %0"
+				: "+m" (this->value));
+	return;
+}
+/************************************************************************/
+/*					      加减后返回新值
+/*                        IncTest DecTest
+/************************************************************************/
+inline int Atomic::AddRet(int Value)
+{
+	Add(Value);
+	return value;
+}
+
+inline int Atomic::SubRet(int Value)
+{
+	Sub(Value);
+	return value;
+}
+
+inline int Atomic::IncRet()
+{
+	Inc();
+	return value;
+}
+
+inline int Atomic::DecRet()
+{
+	Dec();
+	return value;
+}
+/************************************************************************/
+/*					       位图操作
+/*					SetBit ClearBit TestBit
+/************************************************************************/
+inline void Atomic::SetBit(u8 Bit)
+{
+	int mask = 1 << Bit;
+	asm volatile("orl %0,%1"
+				:
+				: "r" (mask), "m" (value)
+				: "memory");
+	return;
+
+}
+
+inline void Atomic::ClearBit(u8 Bit)
+{
+	int mask = ~(1 << Bit);
+	asm volatile("andl %0,%1"
+				:
+				: "r" (mask), "m" (value)
+				: "memory");
+	return;
+
+}
+inline bool Atomic::TestBit(u8 Bit)
+{
+	int mask = 1 << Bit;
+	asm volatile("andl %0,%1"
+				:
+				: "r" (value),"m" (mask)
+				: "memory");
+	return mask;
+
+}
 #endif
