cb6b20ec1e208e022bd87b324cdffc2ca9957344
greenleaf <greenleaf@greenleaf-Veriton-X2610>
2012-07-13 22:37:31 +0800

    60%-thread

--- micro-kernel/Thread.cc
+++ micro-kernel/Thread.cc
@@ -6,13 +6,13 @@
 #include "Thread.h"
 
 using namespace Thread;
-/*===============================================================
- *                        数据区
- ===============================================================*/
-u32 			Thread::kernel_esp;
-KernelThread* 	Thread::run_thread;
-ThreadList 		Thread::ready_thread;
-ThreadList 		Thread::sleep_thread;
+/************************************************************************/
+/*							数据区
+/************************************************************************/
+BmpArray<KernelThread*,NR_THREAD> 	Thread::thread_addr_table;
+KernelThread* 						Thread::run_thread;
+DoubleLink<NR_THREAD>				Thread::ready_thread;
+DoubleLink<NR_THREAD> 				Thread::sleep_thread;
 
 /************************************************************************/
 /*							创建线程
@@ -24,28 +24,34 @@ Result Thread::Create(ThreadFun fun,point params,u16 &ret_id)
 	struct kernel_thread_desc new_thread;
 
 	/* 向内存分配器申请一个页的空间 */
-	/* 未完成 */
 	KernelThread *new_space;
+	/* 未完成 */
+
+	/* 在线程地址表分配一个指针 */
+	int id = 0;
+	int result = thread_addr_table.Add(id,new_space);
+	if (result != S_OK) return result;
 
 	/* 初始化线程描述符 */
+	new_thread.id = id;
 	new_thread.state = THREAD_STATE_READY;
 	new_thread.rec_des = 0;
 	new_thread.save_fun = Thread::Save;
 	new_thread.restart_fun = Thread::Restart;
 
 	/* 并在就绪线程链表中添加一个项 */
-	ready_thread.AddHead(new_space);
+	ready_thread.AddTail(id);
+	ret_id = id;
 
-	/* 初始化栈 */
-	new_thread.id = ready_thread.head;
-	ret_id = ready_thread.head;
+	/* 线程描述符写入在线程空间中 */
 	new_space->thread_info = new_thread;
 
 	/* 函数地址和参数压入线程的栈 */
 	new_space->thread_info.reg.eip = fun;
+
 	/* 把esp的值设为栈顶并把参数压栈 */
-	asm volatile("pushl %%edx			\n"		/* 保存内核栈esp */
-				"movl %%esp,%%edx		\n"
+	asm volatile("pushl %%edx			\n"
+				"movl %%esp,%%edx		\n"		/* 保存内核栈esp */
 				"movl %1,%%esp			\n"		/* esp指向线程栈顶 */
 				"pushl %2				\n"		/* 参数入栈 */
 				"movl %%esp,%0			\n"		/* esp保存到reg */
@@ -55,54 +61,85 @@ Result Thread::Create(ThreadFun fun,point params,u16 &ret_id)
 				:"r"((&new_space)+1)						/* %1,线程栈顶 */
 				 "r"(params)								/* %2,参数 */
 				);
-	return;
+
+	return S_OK;
 }
 
+/************************************************************************/
+/*							睡眠线程
+/*                           Sleep
+/************************************************************************/
+Result Thread::Sleep(u16 id)
+{
+	KernelThread* thread;
+	if(thread_addr_table.Get(id,thread)) return E_NOITEM;
+
+	/* 如果处于运行态,保存上下文,（通过调用Save()函数） */
+	if (run_thread->thread_info.id == id)
+	{
+		CALL_SAVE(run_thread->thread_info)
+		run_thread = '\0';
+		Schedule();
+	}
+
+	/* 把线程的状态设为睡眠态 */
+	thread->thread_info.state = THREAD_STATE_SLEEP;
+
+	/* 从就绪中移除 */
+	if(ready_thread.Search(id))
+		ready_thread.DeleteNext(id);
+
+	/* 加入睡眠队列 */
+	sleep_thread.AddTail(id);
+
+	return S_OK;
+}
+/************************************************************************/
+/*							唤醒线程
+/*                           Wake
+/************************************************************************/
+Result Thread::Wake(u16 id)
+{
+	KernelThread* thread;
+	if(thread_addr_table.Get(id,thread)) return E_NOITEM;
+
+	/* 把线程的状态设为就绪态 */
+	thread->thread_info.state = THREAD_STATE_READY;
+
+	/* 从睡眠队列中移除 */
+	sleep_thread.DeleteNext(id);
+
+	/* 加入就绪队列 */
+	ready_thread.AddTail(id);
+
+	return S_OK;
+}
 /************************************************************************/
 /*							杀死线程
 /*                           Kill
 /************************************************************************/
 Result Thread::Kill(u16 id)
 {
-	KernelThread* obj;
-	KernelThread* tmp;
-
-	ready_thread.Get(id,obj);
+	/* 把线程从线程地址表中移除 */
+	thread_addr_table.Delete(id);
 
 	/* 把线程从任何一个队列中移除 */
-	while(run_thread == obj)
+	if(run_thread == obj)
 	{
 		run_thread = '\0';
 	}
-l_ready:
-	while(ready_thread.Get(tmp) == S_OK)
+	else if(ready_thread.Search(id))
 	{
-		if (tmp == obj)
-		{
-			ready_thread.Delete();
-			goto l_recovery;
-		}
-		if (ready_thread.MoveNext() != S_OK)
-		{
-			goto l_sleep;
-		}
+		ready_thread.DeleteNext(id);
 	}
-l_sleep:
-	while(sleep_thread.Get(tmp) == S_OK)
+	else if(sleep_thread.Search(id))
 	{
-		if (tmp == obj)
-		{
-			sleep_thread.Delete();
-			goto l_recovery;
-		}
-		if (sleep_thread.MoveNext() != S_OK)
-		{
-			goto l_recovery;
-		}
+		sleep_thread.DeleteNext(id);
 	}
-l_recovery:
 
 	/* 发送广播消息，通知各个部件撤销资源*/
+	// SendMessage(MOD_FREE_SOURCE,&id);
 	/* 未完成 */
+
 	return S_OK;
 }
--- micro-kernel/include/Thread.h
+++ micro-kernel/include/Thread.h
@@ -37,12 +37,12 @@ typedef struct stack_frame {
 /*==============线程描述符================*/
 struct kernel_thread_desc
 {
-	u16 id;			/* 线程ID */
-	u16 state;		/* 状态信息 */
-	u32 rec_des;	/* 占有资源描述符 */
-	FunAddr save_fun;		/* 专有的保存状态的函数 */
-	FunAddr restart_fun;	/* 专有的恢复状态的函数 */
-	StackFrame reg;	/* 寄存器值 */
+	u16 		id;				/* 线程ID */
+	u16 		state;			/* 状态信息 */
+	u32 		rec_des;		/* 占有资源描述符 */
+	FunAddr 	save_fun;		/* 专有的保存状态的函数 */
+	FunAddr 	restart_fun;	/* 专有的恢复状态的函数 */
+	StackFrame 	reg;			/* 寄存器值 */
 };
 #define THREAD_STATE_RUNNING 1
 #define THREAD_STATE_READY 2
@@ -62,56 +62,66 @@ typedef union thread_union
 /*==============Thread命名空间==================*/
 namespace Thread
 {
-/*************************线程状态控制函数**********************************/
+/***********线程状态控制函数************/
 	/* 创建线程
 	 * 分配一个线程描述符
 	 * 初始化线程描述符
 	 * 并在就绪线程链表中添加一个项
 	 * 初始化栈
-	 * 函数地址和参数压入线程的栈 */
+	 * 函数地址和参数压入线程的栈
+	 * */
 	Result Create(ThreadFun fun,point params,u16 &ret_id);
 
 	/* 睡眠线程
 	 * 如果处于运行态,保存上下文,（通过调用Save()函数）
 	 * 把线程的状态设为睡眠态
 	 * 从就绪或运行队列中移除
-	 * 加入睡眠队列 */
+	 * 加入睡眠队列
+	 * */
 	Result Sleep(u16 id);
 
 	/* 唤醒线程
 	 * 把线程的状态设为就绪态
 	 * 从睡眠队列中移除
-	 * 加入就绪队列 */
+	 * 加入就绪队列
+	 * */
 	Result Wake(u16 id);
 
 	/* 杀死线程
 	 * 把线程从任何一个队列中移除
-	 * 释放线程占有的任何资源(发送广播消息) */
+	 * 释放线程占有的任何资源(发送广播消息)
+	 * */
 	Result Kill(u16 id);
 
 	/* 设置线程的自定义恢复，保存函数
-	 * 这种机制用于支持涉及优先级转换的线程 */
+	 * 这种机制用于支持涉及优先级转换的线程
+	 * */
 	inline Result SetFun(int id,FunAddr save,FunAddr restart);
 
-/***************************仅针对于run_thread***************************/
-/****************缺省函数，在没有制定save_fun,和restart_fun的时候。**********/
+/******仅针对于run_thread*************/
+	#define CALL_SAVE(desc)		\
+			(*desc->save_fun);
+	#define CALL_RESTART(desc)	\
+			(*desc->restart_fun);
 
-	/* 保存run_thread的通用寄存器和部分段寄存器到线程栈 */
+	/* 保存run_thread的通用寄存器和部分段寄存器到线程栈
+	 * */
 	void Save();
-	/* 恢复并跳到线程中执行 */
+
+	/* 恢复并跳到线程中执行
+	 * */
 	void Restart();
 
 
-	/* 调度算法,选择一个线程作为run_thread */
+	/* 调度算法,选择一个线程作为run_thread
+	 * */
 	void Schedule();
 
-/*******************************数据区***********************************/
-	typedef ShareLinkList<KernelThread*,NR_THREAD> ThreadList;
-	typedef BmpArray<struct link_item<KernelThread*>,NR_THREAD> StaticData;
-	extern u32 kernel_esp;
+/**************数据区***************/
+	extern BmpArray<KernelThread*,NR_THREAD> thread_addr_table;
 	extern KernelThread* run_thread;
-	extern ThreadList ready_thread;
-	extern ThreadList sleep_thread;
+	extern DoubleLink<NR_THREAD> ready_thread;
+	extern DoubleLink<NR_THREAD> sleep_thread;
 };
 
 /************************************************************************/
@@ -120,10 +130,10 @@ namespace Thread
 /************************************************************************/
 inline Result Thread::SetFun(int id,FunAddr save,FunAddr restart)
 {
-	KernelThread*  *p_thread;
+	KernelThread *p_thread;
 
 	/* 获取线程指针 */
-	if (ready_thread.GetAddr(id,&p_thread) != S_OK) return E_NOITEM;
+	if (thread_addr_table.Get(id,p_thread) != S_OK) return E_NOITEM;
 
 	/* 设置函数 */
 	LOCK()
@@ -133,8 +143,6 @@ inline Result Thread::SetFun(int id,FunAddr save,FunAddr restart)
 
 	return S_OK;
 }
-
-
 
 
 #endif /* KERNEL_H_ */
